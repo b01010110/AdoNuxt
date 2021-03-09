99212011fd5d5d277212a1d4df08fe2b28660c93
b01010110 <b01010110b@gmail.com>
2021-03-09 18:31:42 +0700

    starting installation of frameworks

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import chalk from 'chalk'
 import figlet from 'figlet'
 import clear from 'clear'
+import { spawnSync } from 'child_process'
+
+const isWinPlatform: boolean = /^win/.test(process.platform)
+const command_npx: string = isWinPlatform ? 'npx.cmd' : 'npx'
 
 clear()
 
-console.log(
-  chalk.magenta(
-    figlet.textSync('AdoNuxt', { horizontalLayout: 'full' })
-  )
-)
+// logo
+console.log(chalk.magenta(figlet.textSync('AdoNuxt')))
+
+// setup NuxtJs
+spawnSync(command_npx, ['create-nuxt-app', 'client'], { stdio: 'inherit' })
+
+
+// setup AdonisJs
+spawnSync(command_npx, ['create-adonis-ts-app', 'server'], { stdio: 'inherit' })
